4bbcd1e801846677ee42d6fbda8b0bc66116767a
Diego27SEN <francesco.moreno@tecsup.edu.pe>
2026-04-15 12:11:09 -0500

    ads

--- Assets/Scripts/SnapshotSystem/Enemy.cs
+++ Assets/Scripts/SnapshotSystem/Enemy.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform target;
+    private NavMeshAgent agent;
+
     void Start()
     {
-
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (target != null)
+        {
+            agent.destination = target.position;
+        }
     }
 }
--- Assets/Scripts/SnapshotSystem/Player.cs
+++ Assets/Scripts/SnapshotSystem/Player.cs
@@ -1,17 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
+    private InputSystem_Actions inputs;
+    private CharacterController controller;
+
+    private Vector2 moveInput;
+
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 200f;
+
     public int str;
     public int dtx;
     public int spd;
+
+    private void Awake()
+    {
+        inputs = new();
+    }
+
+    private void OnEnable()
+    {
+        inputs.Enable();
+        inputs.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        inputs.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+    }
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        OnMove();
+    }
+
+    public void OnMove()
     {
 
     }
